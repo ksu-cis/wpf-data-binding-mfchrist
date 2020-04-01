657e78f210933b427c07f24b290fa7a37d021762
Mark Christiansen <mfchrist@ksu.edu>
2020-04-01 17:44:13 -0500

    Lab Complete

--- UniversityRegistry/Data/Person.cs
+++ UniversityRegistry/Data/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace UniversityRegistry.Data
@@ -7,8 +8,13 @@ namespace UniversityRegistry.Data
     /// <summary>
     /// A class representing a person associated with the university
     /// </summary>
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Event triggered when properties of Person change
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// The next ID to assign to a newly-created person
         /// </summary>
@@ -22,27 +28,81 @@ namespace UniversityRegistry.Data
         /// <summary>
         /// The person's first name
         /// </summary>
-        public string FirstName { get; set; }
+        private string firstName;       
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName == value) return;
+                firstName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FirstName"));
+            }
+        }
 
         /// <summary>
         /// The person's last name
         /// </summary>
-        public string LastName { get; set; }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName == value) return;
+                lastName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LastName"));
+            }
+        }
+
 
         /// <summary>
         /// The person's date of birth
         /// </summary>
-        public DateTime DateOfBirth { get; set; }
+        private DateTime dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set
+            {
+                if (dateOfBirth == value) return;
+                dateOfBirth = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DateOfBirth"));
+            }
+        }
+
 
         /// <summary>
         /// If this person is active in the university (currently a part of the university)
         /// </summary>
-        public bool Active { get; set; }
+        private bool active;
+        public bool Active
+        {
+            get { return active; }
+            set
+            {
+                if (active == value) return;
+                active = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Active"));
+            }
+        }
+
 
         /// <summary>
         /// The person's role
         /// </summary>
-        public Role Role { get; set; }
+        private Role role;
+        public Role Role
+        {
+            get { return role; }
+            set
+            {
+                if (role == value) return;
+                role = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Role"));
+            }
+        }
+      
         
         /// <summary>
         /// Creates a new user, assigning them an ID
@@ -51,5 +111,16 @@ namespace UniversityRegistry.Data
         {
             ID = NextID++;
         }
+
+        /// <summary>
+        /// Returns a string identifying the person
+        /// </summary>
+        /// <returns>A string consisting of last name, first name, and ID</returns>
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} [{ID}]";
+        }
+
+
     }
 }
